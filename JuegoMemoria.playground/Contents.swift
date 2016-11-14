ef88a9d0d29f6416eecb1ea3167d83cd7b5a26d4
mutaC0d3r <myku@MacBook-Pro-de-myku.local>
2016-11-13 19:21:39 -0600

    Final

--- JuegoMemoria.playground/Contents.swift
+++ JuegoMemoria.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
+for number in 0...100{
+    if number >= 30 && number <= 40{
+        print("# \(number) Viva Swift!!!")
+    }
+    
+    if number%5 == 0 && number != 0 {
+        print("# \(number) Bingo!!!")
+    }
+    
+    if number%2 == 0 {
+        print("# \(number) par")
+    } else {
+        print("# \(number) impar")
+    }
+}
